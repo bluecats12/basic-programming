5a99e99b2caf24885906377b8844a923bd7e66a3
bluecats12 <crepe1408@gmail.com>
2024-07-19 11:41:07 +0700

    เสร็จแล้วจ้า

--- TS index.ts
+++ TS index.ts
@@ -1,1 +1,11 @@
 const fullname : string = 'ธญาวัตร ธีระบำรุงสวัสดิ์'
+
+let x : number =10
+
+let y : number =20
+
+let personpiper : boolean = false
+
+console.log(x==y)
+console.log(x>y)
+console.log(x<=y)
