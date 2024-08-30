1b35060a162ba612f91c5839374d9dc7c8241bfe
bluecats12 <crepe1408@gmail.com>
2024-08-30 11:16:22 +0700

    arry

---
+++ ep5/inde.ts
@@ -0,0 +1,21 @@
+// const animal: string = 'แมว*หมา*นก*หนู'
+
+// const animalArray = animal.split('*')
+// console.log(animalArray)
+// animalArray.pop()
+// console.log(animalArray)
+// animalArray.push('จิงโจ้')
+// console.log(animalArray)
+// animalArray.shift()
+// console.log(animalArray)
+// animalArray.unshift('ไก่')
+// console.log(animalArray)
+
+//const fruits = ["Banana","Orang","Apple","Mango"]
+
+// //const me = {
+//     firtName : "thayawat",
+//     lastName : 'theerabumrungsawat',
+//     age : 21
+// }
+// console.log(me)
